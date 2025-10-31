421f25f62e0bc3dde25be1c6cf9968e76f8004e3
Lemon-ooo <phlong.1112@gmail.com>
2025-10-31 11:30:55 +0700

    service

--- .history/backend/routes/api_20251030233013.php
+++ .history/backend/routes/api_20251030233013.php
@@ -1,10 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\RoomController; // ✅ Thêm \Api
-use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('rooms', RoomController::class);
 Route::get('/room-types', [RoomController::class, 'getRoomTypes']);
 Route::get('/amenities', [RoomController::class, 'getAmenities']);
-Route::apiResource('services', ServiceController::class);
